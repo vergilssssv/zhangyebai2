1af103a7957afa34d5c254c2e305fd7186540c6a
vergilssssv <93860378+vergilssssv@users.noreply.github.com>
2021-11-22 18:15:36 +0800

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,26 +1,47 @@
-window.open=setBGC();
+var i=0;
 function setBGC() {
-	
-	var hour = t.getHours();
-	switch(hour){
-		case 10:document.getElementById("imgBg").style.background = "#0000ff";
+	i++;
+	switch(i){
+			case 1:document.getElementById("imgBg").style.background = "#ff0000";
 			break;
-		case 11:document.getElementById("imgBg").style.background = "#00ff00";
+			case 2:document.getElementById("imgBg").style.background = "#800000";
 			break;
-		case 12:document.getElementById("imgBg").style.background = "#800000";
+			case 3:document.getElementById("imgBg").style.background = "#00ff00";
 			break;
-		case 13:document.getElementById("imgBg").style.background = "#00ffff";
+			case 4:document.getElementById("imgBg").style.background = "#008000";
 			break;
-		case 14:document.getElementById("imgBg").style.background = "#000080";
+			case 5:document.getElementById("imgBg").style.background = "#0000ff";
 			break;
-		case 15:document.getElementById("imgBg").style.background = "#008000";
+			case 6:document.getElementById("imgBg").style.background = "#000080";
 			break;
-		case 16:document.getElementById("imgBg").style.background = "#800000";
+			case 7:document.getElementById("imgBg").style.background = "#ff00ff";
 			break;
-		case 17:document.getElementById("imgBg").style.background = "#00ffff";
+			case 8:document.getElementById("imgBg").style.background = "#ffff00";
 			break;
-		case 18:document.getElementById("imgBg").style.background = "#ffff00";
+			case 9:document.getElementById("imgBg").style.background = "#00ffff";
 			break;
+			case 10:document.getElementById("imgBg").style.background = "#808080";
+			break;
+			case 11:document.getElementById("imgBg").style.background = "#800080";
+			break;
+			case 12:document.getElementById("imgBg").style.background = "#808000";
+			break;
+			case 13:document.getElementById("imgBg").style.background = "#008080";
+			break;
+			case 14:document.getElementById("imgBg").style.background = "#ffc0c0";
+			break;
+			case 15:document.getElementById("imgBg").style.background = "#c0ffc0";
+			break;
+			case 16:document.getElementById("imgBg").style.background = "#c0c0ff";
+			break;
+			case 17:document.getElementById("imgBg").style.background = "#c0ffff";
+			break;
+			case 18:document.getElementById("imgBg").style.background = "#ffc0ff";
+			break;
+			case 19:document.getElementById("imgBg").style.background = "#ffffc0";
+			break;
+			default:i=0;
+
 	}
 	
 	}
